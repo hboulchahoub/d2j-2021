ea9a8825357e1e6c29f63f6430843920f072c493
LENOVO GAMER <LENOVO GAMER@DESKTOP-SSNOUCT>
2020-12-26 18:25:01 +0000

    course 26/12/2020: 1- adding hibernate dependencies at pom.xml 2- adding mapping ORM at Demand class 3- adding jpa config file persistence.xml (META-INF) 4- adding entitymanager attribute at DemandeDaoImpl class

--- PFE/src/main/java/ma/cigma/pfe/module1/dao/DemandDaoImpl.java
+++ PFE/src/main/java/ma/cigma/pfe/module1/dao/DemandDaoImpl.java
@@ -1,5 +1,9 @@
 package ma.cigma.pfe.module1.dao;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import org.springframework.stereotype.Repository;
 
 import ma.cigma.pfe.module1.models.Demand;
@@ -7,19 +11,31 @@ import ma.cigma.pfe.module1.models.Demand;
 /**
  * this class is created to update the database
  * and it will implement all CRUD  operations
- * It is responsible of all access to database.
+ * It is responsible for database access.
  * will use JPA to implement it.
  * @author Mr.BOULCHAHOUB
  * @date 21.12.2020
  */
 @Repository
 public class DemandDaoImpl implements IDemandDao {
+	private EntityManagerFactory emf= 
+	Persistence.createEntityManagerFactory("abc");
+	private EntityManager em=emf.createEntityManager();
 	@Override
 	public boolean insert(Demand d) {
-		System.out.println("Calling dao Insert...");
+		// Transaction
+		em.getTransaction().begin();
+		em.persist(d);
+		em.getTransaction().commit();
 		return false;
 	}
-
+	public static void main(String[] args) {
+		IDemandDao dao = new DemandDaoImpl();
+		Demand d= new Demand();
+		d.setCompte("A200");
+		d.setNb(30);
+		dao.insert(d);
+	}
 	@Override
 	public boolean update(Demand d) {
 		return false;
--- PFE/src/main/java/ma/cigma/pfe/module1/models/Demand.java
+++ PFE/src/main/java/ma/cigma/pfe/module1/models/Demand.java
@@ -1,19 +1,45 @@
 package ma.cigma.pfe.module1.models;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
 /**
- * this class is created to contain data introduced by users
- * Objects form this class will persisted in our data base
- * this class must contain only getter, setters, constructors
- * it can also override methods form Object class.
+ * this class is created to contain data introduced by users Objects form this
+ * class will persisted in our data base this class must contain only getter,
+ * setters, constructors it can also override methods form Object class.
+ * 
  * @author Mr.BOULCHAHOUB
  * @date 21.12.2020
  */
+@Entity
+@Table(name = "TDemands")
 public class Demand {
-	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	//Identity = AUTOINCREMENT
+	//SEQUENCE = UTILISE UNE SEQUENCE [SEQUNCE SGBD]
+	//TABLE = hibernate_sequences
+	//AUTO= CHOISIR UNE DES TROIS
+	private int id;
+	@Column(name="cmp")
 	private String compte;
 	private int nb;
+	@Transient
+	int age;
 
-	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
 	public String getCompte() {
 		return compte;
 	}
